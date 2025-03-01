af3ed1c8622edd7972f3bde06d55723b3964e5f9
naziachowdhurydev <naziachowdhurydev@gmail.com>
2025-03-01 20:44:21 +0600

    pages settings

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,47 @@ import "./singlePost.css";
 export default function SinglePost() {
   return (
     <>
-      <div className="singlePost">single post</div>
+      <div className="singlePost">
+        <div className="singlePostWrapper">
+          <img
+            className="singlePostImg"
+            src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+          />
+          <h1 className="singlePostTitle">
+            Lorem ipsum dolor sit amet.
+            <div className="singlePostEdit">
+              <i className="singlePostIcon fa-regular fa-pen-to-square"></i>
+              <i className="singlePostIcon fa-regular fa-trash-can"></i>
+            </div>
+          </h1>
+          <div className="singlePostInfo">
+            <span className="singlePostAuthor">
+              Author: <b>Safak</b>
+            </span>
+            <span className="singlePostDate">1 hour ago</span>
+          </div>
+          <p className="singlePostDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quidem
+            nesciunt consequuntur, tenetur doloribus eligendi quaerat iusto
+            dolorum neque beatae doloremque autem magnam similique minus maiores
+            delectus illum dicta, dolore ab debitis? Hic commodi eligendi
+            similique consectetur mollitia, vero repellat odio alias explicabo
+            incidunt dolorum deleniti repudiandae molestias tempora sequi eaque
+            quidem necessitatibus laborum ea? Sequi corporis rem expedita minima
+            ullam odio ducimus placeat eum inventore laboriosam pariatur,
+            dignissimos esse veritatis eligendi quia unde optio officiis
+            suscipit reprehenderit deserunt nobis similique iste quam! Quis, non
+            deleniti reiciendis et expedita, aliquam, modi temporibus
+            repudiandae ex perspiciatis libero numquam ab. Nam, maxime? placeat
+            eum inventore laboriosam pariatur, dignissimos esse veritatis
+            eligendi quia unde optio officiis suscipit reprehenderit deserunt
+            nobis similique iste quam! Quis, non deleniti reiciendis et
+            expedita, aliquam, modi temporibus repudiandae ex perspiciatis
+            libero numquam ab. Nam, maxime?
+          </p>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,36 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./settings.css";
+
+export default function Settings() {
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <lable className="fileInput">
+              <i className="settingsPPIcon fa-regular fa-user"></i>
+            </lable>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Safak" />
+          <label>Emai;</label>
+          <input type="email" placeholder="safak@gmail.co," />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
